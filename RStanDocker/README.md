e49f0192b8a05911bc8a412e88d323d149b03a6d
Richard Erickson <rerickson@usgs.gov>
2017-03-15 12:16:20 -0500

    Added example dockerfile with Rstan

---
+++ RStanDocker/README.md
@@ -0,0 +1,4 @@
+## RStan Dockerfile
+
+This file contains a dockerfile to build a docker container with RStan. 
+
---
+++ RStanDocker/test.R
@@ -0,0 +1,5 @@
+library('rstan')
+
+nObs <- 10
+d <- data.frame(x = 1:nObs, y = rnorm(n = nObs, mean = 1, sd = 2))
+write.csv(file = "/opt/rerickson/xout.csv", x = d, row.names = FALSE)
